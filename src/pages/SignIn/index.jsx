b5a867afa969ce0a601c7c3392ed2e1c5db7daff
Vinícius <santsmarcos81@gmail.com>
2024-09-23 21:06:31 -0300

    Atualizacao link api

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -4,7 +4,6 @@ import { FiMail, FiLock } from 'react-icons/fi';
 import { useAuth } from '../../hooks/auth';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
-import logo from '../../assets/logo.png';
 
 import { Container, Form, Background } from './styles';
 
@@ -25,7 +24,6 @@ export function SignIn() {
         <Container>
             <Background />
             <Form>
-                <img src={logo} />
                 <h2>Acesse sua conta</h2>
 
                 <Input 
